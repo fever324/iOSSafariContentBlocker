3ae63bc31a1400d656d0ff44bc29cc2767eac30f
Hongfei Li <fever324@gmail.com>
2016-04-01 00:58:37 -0400

    Initial Commit

---
+++ SuperEvil/ActionRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ActionRequestHandler.swift
+//  SuperEvil
+//
+//  Created by Hongfei Li on 4/1/16.
+//  Copyright © 2016 Hongfei Li. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+    
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+    
+        context.completeRequestReturningItems([item], completionHandler: nil);
+    }
+    
+}
